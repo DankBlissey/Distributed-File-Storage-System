c0e89db165c9d21c7090c7f33196c2b44428ce77
jh11g20 <jh11g20@soton.ac.uk>
2023-06-02 17:21:24 +0100

    Adjusted the fakeclient to be able to send any file type of any size

--- src/FakeClient.java
+++ src/FakeClient.java
@@ -6,35 +6,36 @@ import java.nio.file.Paths;
 public class FakeClient {
     public static void main(String [] args) {
         Socket socket = null;
-        byte[] fileBytes = null;
+        byte[] fileBytes;
         String path = args[1];
         String name = args[2];
         try {
             fileBytes = Files.readAllBytes(Paths.get(path));
-        } catch (Exception e) {
-            System.err.println("Error" + e);
-        }
-        try {
-            InetAddress address = InetAddress.getLocalHost();
-            socket = new Socket(address, Integer.parseInt(args[0]));
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            assert fileBytes != null;
-            out.println("STORE " + name + " 1Kb");
-            System.out.println("Requesting to store file");
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            String reply;
-            reply = in.readLine();
+            try {
+                InetAddress address = InetAddress.getLocalHost();
+                socket = new Socket(address, Integer.parseInt(args[0]));
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                //assert fileBytes != null;
+                out.println("STORE " + name + " 1Kb");
+                System.out.println("Requesting to store file");
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String reply;
+                reply = in.readLine();
                 if(reply.equals("ACK")) {
                     System.out.println("Request acceptance received, sending file data");
-                    //Thread.sleep(2000);
+                    //Thread.sleep(1000);
                     socket.getOutputStream().write(fileBytes);
                     System.out.println("Data Sent");
                 }
 
-        } catch(Exception e) { System.err.println("error: " + e);
-        } finally {
-            if (socket != null)
-                try { socket.close(); } catch (IOException e) { System.err.println("error: " + e); }
+            } catch(Exception e) { System.err.println("error: " + e);
+            } finally {
+                if (socket != null)
+                    try { socket.close(); } catch (IOException e) { System.err.println("error: " + e); }
+            }
+        } catch (Exception e) {
+            System.err.println("Error" + e);
         }
+
     }
 }
