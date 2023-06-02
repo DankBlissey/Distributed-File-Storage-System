a025310256a60b13c3059ff705afd5dca9fd52f8
jh11g20 <jh11g20@soton.ac.uk>
2023-06-02 11:17:28 +0100

    Added simple code that enables a file to be sent from a test client to the Dstore and be stored in the specified Dstore location

--- src/Dstore.java
+++ src/Dstore.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.net.*;
+import java.nio.file.Path;
 
 public class Dstore {
     public static void main (String [] args) {
@@ -7,18 +8,13 @@ public class Dstore {
         ServerSocket cport = null;
         Integer timeout = null;
         String fileFolderTxt = null;
-        File fileFolder = null;
+        Socket client = null;
         try {
             port = new ServerSocket(Integer.parseInt(args[0]));
             cport = new ServerSocket(Integer.parseInt(args[1]));
             timeout = Integer.parseInt(args[2]);
             fileFolderTxt = args[3];
-            try {
-                fileFolder = new File(fileFolderTxt);
-            } catch (Exception c) {
-                System.err.println("error" + c);
-            }
-            while(true) {
+                /*
                 try {
                     Socket client = port.accept();
                     BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
@@ -26,12 +22,46 @@ public class Dstore {
                     while((line = in.readLine()) != null) System.out.println(line+" received");
                     client.close();
                 } catch(Exception e) { System.err.println("error: " + e); }
+
+                 */
+        } catch (Exception e) {
+            System.err.println("Issues with Dstore setup: " + e);
+        }
+
+
+        try {
+            client = port.accept();
+        } catch (Exception e) {
+            System.err.println("Socket Accept failed: ");
+        }
+        StoreFile(client, fileFolderTxt);
+    }
+
+    public static void StoreFile(Socket client, String fileFolderTxt) {
+        try {
+            System.out.println("Client Connected: " + client.getInetAddress().getHostAddress());
+            InputStream inputStream = client.getInputStream();
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            String fileName = "recievedFile.txt";
+            Path destinationPath = Path.of(fileFolderTxt + fileName);
+            FileOutputStream fileOutputStream = new FileOutputStream(destinationPath.toString());
+
+            while ((bytesRead = inputStream.readNBytes(buffer, 0, buffer.length)) > 0) {
+                fileOutputStream.write(buffer, 0, bytesRead);
             }
 
+            fileOutputStream.close();
 
+            System.out.println("File recieved and saved to: " + fileFolderTxt);
         } catch (Exception e) {
-            System.err.println("Issues with Dstore setup" + e);
+            System.err.println("error: " + e);
         }
     }
+
+    public static void ReceiveRequest() {
+
+    }
+
 
 }
--- src/FakeClient.java
+++ src/FakeClient.java
@@ -1,17 +1,24 @@
 import java.io.*;
 import java.net.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class FakeClient {
     public static void main(String [] args) {
         Socket socket = null;
+        byte[] fileBytes = null;
+        try {
+            fileBytes = Files.readAllBytes(Paths.get("C:/Users/John/Documents/TestFile.txt"));
+        } catch (Exception e) {
+            System.err.println("Error" + e);
+        }
         try {
             InetAddress address = InetAddress.getLocalHost();
             socket = new Socket(address, Integer.parseInt(args[0]));
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            for(int i = 0; i < 10; i++) {
-                out.println("TCP message "+i); //out.flush();
-                Thread.sleep(1000);
-            }
+            assert fileBytes != null;
+            socket.getOutputStream().write(fileBytes);
+
         } catch(Exception e) { System.err.println("error: " + e);
         } finally {
             if (socket != null)
