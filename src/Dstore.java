a4e3578d8acea7457cd9d37c36a6b9a6d440168f
jh11g20 <jh11g20@soton.ac.uk>
2023-06-02 13:00:04 +0100

    Added code so that the Dstore expects a request with the file name and size, then replies to accept, then recieves the file and stores it. Also implemented a timeout if the file is not sent after the acceptance message within the timeout

--- src/Dstore.java
+++ src/Dstore.java
@@ -1,49 +1,42 @@
 import java.io.*;
 import java.net.*;
 import java.nio.file.Path;
+import java.util.Objects;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Dstore {
+    static ServerSocket port = null;
+    static ServerSocket cport = null;
+    static Integer timeout = null;
+    static String fileFolderTxt = null;
+    static Socket client = null;
     public static void main (String [] args) {
-        ServerSocket port = null;
-        ServerSocket cport = null;
-        Integer timeout = null;
-        String fileFolderTxt = null;
-        Socket client = null;
         try {
             port = new ServerSocket(Integer.parseInt(args[0]));
             cport = new ServerSocket(Integer.parseInt(args[1]));
             timeout = Integer.parseInt(args[2]);
             fileFolderTxt = args[3];
-                /*
-                try {
-                    Socket client = port.accept();
-                    BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-                    String line;
-                    while((line = in.readLine()) != null) System.out.println(line+" received");
-                    client.close();
-                } catch(Exception e) { System.err.println("error: " + e); }
-
-                 */
         } catch (Exception e) {
             System.err.println("Issues with Dstore setup: " + e);
         }
 
-
         try {
+            assert port != null : "Connection is null:";
             client = port.accept();
+            System.out.println("Client connected:");
         } catch (Exception e) {
             System.err.println("Socket Accept failed: ");
         }
-        StoreFile(client, fileFolderTxt);
+        ReceiveRequest(client);
     }
 
-    public static void StoreFile(Socket client, String fileFolderTxt) {
+    public static void StoreFile(Socket client, String fileFolderTxt, String fileName) {
         try {
             System.out.println("Client Connected: " + client.getInetAddress().getHostAddress());
             InputStream inputStream = client.getInputStream();
             byte[] buffer = new byte[1024];
             int bytesRead;
-            String fileName = "recievedFile.txt";
             Path destinationPath = Path.of(fileFolderTxt + fileName);
             FileOutputStream fileOutputStream = new FileOutputStream(destinationPath.toString());
 
@@ -52,6 +45,7 @@ public class Dstore {
             }
 
             fileOutputStream.close();
+            client.close();
 
             System.out.println("File recieved and saved to: " + fileFolderTxt);
         } catch (Exception e) {
@@ -59,8 +53,38 @@ public class Dstore {
         }
     }
 
-    public static void ReceiveRequest() {
-
+    public static void ReceiveRequest(Socket client) {
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            String line;
+            while((line = in.readLine()) != null) {
+                String[] lines = line.split(" ");
+                if(Objects.equals(lines[0], "STORE") && lines.length == 3) {
+                    System.out.println("Storage request recieved:");
+                    PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+                    out.println("ACK");
+                    Timer timer = new Timer();
+                    timer.schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            // Timeout occurred, handle it
+                            System.out.println("Timeout occurred. Closing connection.");
+                            try {
+                                in.close();
+                                out.close();
+                                client.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }, timeout);
+                    timer.cancel();
+                    StoreFile(client, fileFolderTxt, lines[1]);
+                }
+            }
+        } catch (Exception e) {
+            System.err.println("Confirmation of storage failed: ");
+        }
     }
 
 
--- src/FakeClient.java
+++ src/FakeClient.java
@@ -17,7 +17,17 @@ public class FakeClient {
             socket = new Socket(address, Integer.parseInt(args[0]));
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
             assert fileBytes != null;
-            socket.getOutputStream().write(fileBytes);
+            out.println("STORE TestFile.txt 1Kb");
+            System.out.println("Requesting to store file");
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String reply;
+            while((reply = in.readLine()) != null) {
+                if(reply.equals("ACK")) {
+                    System.out.println("Request acceptance received, sending file data");
+                    socket.getOutputStream().write(fileBytes);
+                    System.out.println("Data Sent");
+                }
+            }
 
         } catch(Exception e) { System.err.println("error: " + e);
         } finally {
